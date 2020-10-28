75a40712574bb58a196f2a6dd73b507e40fe5b0e
Ilya Yushkevich <ilyshka88@gmail.com>
2020-10-28 22:01:34 +0300

    lab5 done up to 3rd point

---
+++ lab5/lab5/Mammals Heirs.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace lab5
+{
+    public class Lion: Mammals, IAnimal
+    {
+        public Lion() { }
+        public override void eat()
+        {
+            Console.WriteLine("Lion drinks milk.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Lion moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Lion sleeps.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    public class Tiger : Mammals, IAnimal
+    {
+        public Tiger() { }
+        public override void eat()
+        {
+            Console.WriteLine("Tiger drinks milk.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Tiger moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Tiger sleeps.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000); //наохотился и спит
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    public class Owl: Bird, IAnimal
+    {
+        public Owl() { }
+        public override void eat()
+        {
+            Console.WriteLine("Owl eats insects.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Owl moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"OWL'S WATCHIN YA.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000); //наохотился и спит
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    public class Croc : IAnimal
+    {
+        int position;
+        public Croc() { }
+        public void eat()
+        {
+            Console.WriteLine("Owl eats insects.");
+        }
+
+        public int move()
+        {
+            position += 1;
+            Console.WriteLine($"Owl moved to {position}.");
+            return position;
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine($"OWL'S WATCHIN YA.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000); //наохотился и спит
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    public class Shark : Fish
+    {
+        public Shark() { }
+        public override void eat()
+        {
+            Console.WriteLine("Shark eats fish.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Shark moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Shark sleeps.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000); //наохотился и спит
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    public sealed class Parrot: Bird
+    {
+        public Parrot() { }
+        public override void eat()
+        {
+            Console.WriteLine("Parrot eat breadcrumbs and drink water.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Parrot flies to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Parrot sleeps.");
+        }
+
+        public void hunt()
+        {
+            Console.WriteLine("Hunt in progress...");
+            System.Threading.Thread.Sleep(1000); //наохотился и спит
+            Console.WriteLine("Hunt complete!");
+        }
+    }
+
+    //public class AnotherTypeOfParrot: Parrot { } Попугай запечатан, поэтоиу невозможно создать производный класс
+}
---
+++ lab5/lab5/Mammals.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace lab5
+{
+    public abstract class Animal
+    {
+        protected int position;
+        public abstract int move();
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal eats.");
+        }
+        public abstract void sleep();
+    }
+
+    public interface IAnimal
+    {
+        int move();
+        void eat();
+        void sleep();
+        void hunt()
+        {
+            Console.WriteLine("Animal hunts");
+        }
+
+    }
+
+    public abstract class Mammals: Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Mammal drinks milk.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Mammal moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Mammal sleeps.");
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Birds eat breadcrumbs and drink water.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Bird moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Bird sleeps.");
+        }
+    }
+
+    public class Fish : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Fish eats dunno what.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Fish moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Fish sleeps.");
+        }
+    }
+}
