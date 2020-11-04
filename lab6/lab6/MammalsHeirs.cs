6c14e82b8a57cfb3471ccac98c6e046c368e6f2e
Ilya Yushkevich <ilyshka88@gmail.com>
2020-11-04 22:38:02 +0300

    lab6 1-2 done. 3 WIP

---
+++ lab6/lab6/MammalsHeirs.cs
@@ -0,0 +1,155 @@
+﻿using System;
+namespace lab6
+{
+    public abstract class Animal
+    {
+        protected int position;
+        public abstract int move();
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal eats.");
+        }
+        public abstract void sleep();
+        public abstract void a_method();
+    }
+
+    public interface IAnimal
+    {
+        int move();
+        void eat();
+        void sleep();
+        void hunt()
+        {
+            Console.WriteLine("Animal hunts");
+        }
+        void a_method();
+
+    }
+
+    public class Mammals : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Mammal drinks milk.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Mammal moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Mammal sleeps.");
+        }
+
+        public override void a_method()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return @$"Info:
+                    Position: {position}
+                    Object type: Mammals";
+        }
+
+    }
+
+    public class Bird : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Birds eat breadcrumbs and drink water.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Bird moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Bird sleeps.");
+        }
+
+        public override void a_method()
+        {
+
+        }
+    }
+
+    public class Fish : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Fish eats dunno what.");
+        }
+
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Fish moved to {position}.");
+            return position;
+        }
+
+        public override void sleep()
+        {
+            Console.WriteLine($"Fish sleeps.");
+        }
+
+        public override void a_method()
+        {
+
+        }
+    }
+
+    public partial class Lion
+    {
+        public override int move()
+        {
+            position += 1;
+            Console.WriteLine($"Lion moved to {position}.");
+            return position;
+        }
+    }
+
+    public class Zoo
+    {
+        Animal[] zoo;
+        int zooSize;
+        public Zoo()
+        {
+            Console.WriteLine("Enter the size of the zoo");
+            zooSize = Console.Read();
+            Animal[] zoo = new Animal[zooSize];
+        }
+
+        public void Add(Animal obj)
+        {
+            zoo[zoo.Length] = obj;
+        }
+
+        public void Remove(int i)
+        {
+            zoo[i] = null;
+            for(int j = i; j < zoo.Length; j++)
+            {
+                zoo[j] = zoo[j + 1];
+            }
+        }
+
+        public void ShowList()
+        {
+            for (int j = 0; j < zoo.Length; j++)
+            {
+                Console.WriteLine(zoo[j]);
+            }
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion lion1 = new Lion();
+            Tiger tiger1 = new Tiger();
+            NewClass newClass1 = new NewClass();
+            Croc croc1 = new Croc();
+
+            Console.WriteLine("Croc can{0} be converted to object", (croc1 is Object) ? "" : "not");
+            Console.WriteLine("Croc can{0} be converted to ValueType", (croc1 is ValueType) ? "" : "not"); //is - проверка на конвертацию
+
+            Mammals mammal1 = new Mammals();
+            Lion pLion = mammal1 as Lion;
+            Console.WriteLine("Mammals to Lion conversion is {0} completed", (pLion != null) ? "" : "not"); //as - конвертация
+
+            Mammals pTiger = tiger1 as Mammals;
+            Console.WriteLine("Tiger to mammals conversion is {0} completed", (pTiger != null) ? "" : "not"); //as - конвертация
+            Console.WriteLine(pTiger); //после переопределения ToString выводит info
+
+            Console.WriteLine(lion1.ToString());
+
+            Print print1 = new Print();
+            Mammals[] mammalsArr = new Mammals[] { lion1, tiger1 };
+            foreach (var s in mammalsArr)
+            {
+                print1.IAmPrinting(s);
+                Console.WriteLine();
+            }
+
+            NewClass new1 = new NewClass();
+            ((IAnimal)new1).a_method();
+            new1.a_method();
+
+            Smth struct1;
+            struct1.smth = "Smth";
+            Console.WriteLine(struct1.smth);
+            Smth.Month month1;
+            month1 = Smth.Month.January;
+            Console.Write(month1 + " ");
+            Console.WriteLine((int)month1);
+
+
+            Zoo zoo1 = new Zoo();
+            zoo1.Add(tiger1);
+            zoo1.Add(lion1);
+            zoo1.ShowList();
+            zoo1.Remove(1);
+            zoo1.ShowList();
+
+        }
+    }
+}
